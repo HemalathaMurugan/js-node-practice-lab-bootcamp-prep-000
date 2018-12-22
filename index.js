39bd541222647437d84a0c279bd3b8e84f5808de
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-22 01:40:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Hema"
-var height = "65"
+var height = 66
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
 
